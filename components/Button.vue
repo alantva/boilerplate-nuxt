8e446525eb065a26504e826aee39cc02136f50fc
Alan Timossi Farias de Oliveira <alan.oliveira@doc88.com.br>
2019-10-17 16:04:46 -0300

    Button

---
+++ components/Button.vue
@@ -0,0 +1,37 @@
+<template>
+  <button type="button">
+    <slot name="default"></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button'
+}
+</script>
+
+<style lang="scss" scoped>
+@each $c in ('primary', 'secondary') {
+  .button--#{$c} {
+    display: inline-block;
+    border-radius: 4px;
+    border: 1px solid t($theme, $c);
+    color: t($theme, $c);
+    text-decoration: none;
+    cursor: pointer;
+    outline: none;
+    padding: 10px 30px;
+    transition: background-color 0.2s ease-in, color 0.2s ease-in;
+
+    &:hover,
+    &:active {
+      color: t($theme, 'on-' + $c);
+      background-color: t($theme, $c);
+    }
+
+    &:active {
+      border: 1px solid t($theme, 'on-' + $c);
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,32 +3,26 @@
     <div>
       <logo />
       <h1 class="title">
-        boilerplate-vue
+        Boilerplate Nuxt
       </h1>
       <h2 class="subtitle">
         My stellar Nuxt.js project
       </h2>
       <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+        <Button class="button--primary">Documentation</Button>
+        <Button class="button--secondary">GitHub</Button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Button from '~/components/Button'
+import Logo from '~/components/Logo'
 
 export default {
   components: {
+    Button,
     Logo
   }
 }
@@ -48,14 +42,12 @@ export default {
   display: block;
   font-weight: 300;
   font-size: 100px;
-  color: #35495e;
   letter-spacing: 1px;
 }
 
 .subtitle {
   font-weight: 300;
   font-size: 42px;
-  color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
 }
